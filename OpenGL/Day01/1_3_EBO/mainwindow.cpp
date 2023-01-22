f8b04875151f57c415991a4fe8ed3b9d69eb3731
xizhongren <2745308980@qq.com>
2023-01-22 19:35:26 +0800

    OpenGL之EBO的使用

---
+++ OpenGL/Day01/1_3_EBO/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+//绘制矩形
+void MainWindow::on_drawRect_btn_clicked()
+{
+
+}
