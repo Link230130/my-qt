4087dbf8f395dd6ff778a34dc90c82bb3732aeea
xizhongren <2745308980@qq.com>
2023-03-27 22:20:36 +0800

    Linux客户端/服务器通信_v1

---
+++ Linux/Network/Client/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include "unistd.h"
+#include "arpa/inet.h"
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    //1.创建用于通信的套接字
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+    {
+        cout << "socket" << endl;
+    }
+    //2.连接服务器
+    sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(9999);
+    inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr.s_addr);
+    int ret = connect(fd, (sockaddr*)&saddr, sizeof(saddr));
+    if (ret == -1) {
+        cout << "connect error" << endl;
+    }
+
+    //3.套接字通信
+    std::string sendMsg = "";
+    char recvMsg[1024];
+    int number = 0;
+    while (1)
+    {
+        //发送数据
+        sendMsg = ("hello, i am client");
+        cout << "client will send msg to server, msg=" << sendMsg << endl;
+        send(fd, sendMsg.c_str(), sendMsg.length()+1, 0);
+        //接收数据
+        memset(recvMsg, 0, sizeof(recvMsg));
+        int len = recv(fd, recvMsg, sizeof(recvMsg), 0);
+        if (len > 0) {
+            cout << "server say : " << recvMsg << endl;
+        }
+        else if (len == 0) {
+            cout << "disconnect and break" << endl;
+            break;
+        }
+        else {
+            cout << "recv error" << endl;
+            break;
+        }
+        sleep(1);
+    }
+    close(fd);
+    return 0;
+}
---
+++ Linux/Network/Server/main.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include "unistd.h"
+#include "arpa/inet.h"
+#include <string>
+
+using namespace std;
+
+#define IS_DEBUG 1
+
+int main() {
+    //1.创建监听的套接字
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+#if IS_DEBUG
+    cout << "socket succeed!!! and fd=" << fd << endl;
+#endif
+    if (fd == -1)
+    {
+        cout << "socket" << endl;
+        return -1;
+    }
+    //2.绑定本地的IP 、Port
+    struct sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(9999);
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    int ret = bind(fd, (struct sockaddr*)&saddr, sizeof(saddr));
+    if (ret == -1) {
+        cout << "bind" << endl;
+        return -1;
+    }
+#if IS_DEBUG
+    cout << "bind succeed!!! and ret=" << ret << endl;
+#endif
+    //3.设置监听
+    ret = listen(fd, 128);
+    if (ret == -1) {
+        cout << "listen error" << endl;
+        return -1;
+    }
+#if IS_DEBUG
+    cout << "listen succeed!!! and ret=" << ret << endl;
+#endif
+    //4.阻塞并等待客户端的连接
+    struct sockaddr_in caddr;
+    int addrLen = sizeof(caddr);
+    int cfd = accept(fd, (struct sockaddr*)&caddr, (socklen_t*)&addrLen);
+    if (cfd == -1)
+    {
+        cout << "accept error" << endl;
+        return -1;
+    }
+#if IS_DEBUG
+    cout << "accept succeed!!! and cfd=" << cfd << endl;
+#endif
+    //连接建立成功，打印客户端的IP和Port信息
+    char ip[32];
+    cout << "The client IP: " << inet_ntop(AF_INET, &caddr.sin_addr.s_addr, ip, sizeof(ip)) << endl;
+    cout << "The client Port: " << ntohs(caddr.sin_port) << endl;
+
+    //5.与客户端通信
+    while (1)
+    {
+        char buff[1024];
+        int len = recv(cfd, buff, sizeof(buff), 0);
+        if (len > 0)
+        {
+            cout << "client say: " << buff << endl;
+            send(cfd, buff, len, 0);
+        }
+        else if (len == 0)
+        {
+            cout << "buff is 0" << endl;
+            break;
+        }
+        else {
+            cout << "recv error" << endl;
+            break;
+        }
+    }
+    //回收资源
+    close(fd);
+    close(cfd);
+#if IS_DEBUG
+    cout << "close succeed!!!" << endl;
+#endif
+    return 0;
+}
